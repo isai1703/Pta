b827a59e5f3cddee96057adabcfdf3562f525de0
isai1703 <oscar_isai_170596@hotmail.com>
2025-08-26 11:21:57 -0600

    Fix: agregado Gson en dependencias para ProductStorage.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,10 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
 
     // Gson para manejo de JSON
